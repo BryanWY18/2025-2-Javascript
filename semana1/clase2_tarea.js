3d07a7ce9411ff4ebca53b4228c31ba68c55e57c
Bryan <bryanpiga1309@gmail.com>
2025-05-07 12:51:32 -0600

    Act. se añaden °F con sus respectivas temperaturas

--- semana1/clase2_tarea.js
+++ semana1/clase2_tarea.js
@@ -10,6 +10,8 @@
 const { ask } = require('../helpers/input');
 
 async function main() {
+
+  const gradacion = await ask ("Seleccione la escala de temperatura: °C/°F: ")
   const temperatura = Number(await ask("¿Cuál es la temperatura actual? "));
 
   let cold = "Hace frío";
@@ -26,41 +28,80 @@ async function main() {
   let answer3 = "Manténgase hidratado";
   let answer4 = "Evite exponerse al sol";
 
-
-  if (temperatura < 10 && temperatura > 0) {
-    console.log(cold);
-    const recomend = await ask (question);
-      if(recomend === "SI"){
-        console.log(answer1);
-      } else {
-        console.log(noThanks);
-      }
-  } else if (temperatura >= 10 && temperatura <= 20) {
-    console.log(template);
-    recomend = await ask (question);
-    if(recomend === "SI"){
-      console.log(answer2);
+  if(gradacion==="°C"){
+    if (temperatura < 10 && temperatura > 0) {
+      console.log(cold);
+      const recomend = await ask (question);
+          if(recomend === "SI"){
+            console.log(answer1);
+          } else {
+            console.log(noThanks);
+          }
+    } else if (temperatura >= 10 && temperatura <= 20) {
+      console.log(template);
+      recomend = await ask (question);
+        if(recomend === "SI"){
+          console.log(answer2);
+        } else {
+          console.log(noThanks);
+        }
+    } else if (temperatura >= 21 && temperatura <= 30) {
+      console.log(hot);
+      recomend = await ask (question);
+        if(recomend === "SI"){
+          console.log(answer3);
+        } else {
+          console.log(noThanks);
+        }
+    } else if (temperatura > 30) {
+      console.log(veryHot);
+      recomend = await ask (question);
+        if(recomend === "SI"){
+          console.log(answer4);
+        } else {
+          console.log(noThanks);
+        }
     } else {
-      console.log(noThanks);
+      console.log(noData);
     }
-  } else if (temperatura >= 21 && temperatura <= 30) {
-    console.log(hot);
-    recomend = await ask (question);
-    if(recomend === "SI"){
-      console.log(answer3);
+  }else{
+    if (temperatura < 32) {
+      console.log("El frío es extremo, tome precauciones");
+    } else if (temperatura >= 33 && temperatura <=50) {
+      console.log(cold);
+      recomend = await ask (question);
+          if(recomend === "SI"){
+            console.log(answer1);
+          } else {
+            console.log(noThanks);
+          }
+    } else if (temperatura >= 51 && temperatura <= 68) {
+      console.log(template);
+      recomend = await ask (question);
+        if(recomend === "SI"){
+          console.log(answer2);
+        } else {
+          console.log(noThanks);
+        }
+    } else if (temperatura >= 69 && temperatura <= 85) {
+      console.log(hot);
+      recomend = await ask (question);
+        if(recomend === "SI"){
+          console.log(answer3);
+        } else {
+          console.log(noThanks);
+        }
+    } else if (temperatura > 87) {
+      console.log(veryHot);
+      recomend = await ask (question);
+        if(recomend === "SI"){
+          console.log(answer4);
+        } else {
+          console.log(noThanks);
+        }
     } else {
-      console.log(noThanks);
+      console.log(noData);
     }
-  } else if (temperatura > 30) {
-    console.log(veryHot);
-    recomend = await ask (question);
-    if(recomend === "SI"){
-      console.log(answer4);
-    } else {
-      console.log(noThanks);
-    }
-  } else {
-    console.log(noData);
   }
   // TODO: Implementar la lógica para temperaturas entre 10 y 20 grados
   // TODO: Implementar la lógica para temperaturas entre 20 y 30 grados
