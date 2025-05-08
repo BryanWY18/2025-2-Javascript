668e293ff3c8dd5190bae6e97f1ae3994cfa2940
Bryan <bryanpiga1309@gmail.com>
2025-05-08 13:24:10 -0600

    Resuelvo tarea clase 3, switch

--- semana1/ejercicio3.js
+++ semana1/ejercicio3.js
@@ -2,8 +2,9 @@
 const { ask } = require('../helpers/input');
 
 async function main() {
-  const number = Number(prompt("Ingresa un número:"));
+  const number = Number(await ask("Ingresa un número:"));
 
+  /*
   if (number % 3 === 0 && number % 5 === 0) {
     console.log("Múltiplo de 3 y 5");
   } else if (number % 3 === 0) {
@@ -13,17 +14,27 @@ async function main() {
   } else {
     console.log("No es múltiplo de 3 ni de 5");
   }
+    */
 
   // TODO : Convertir a switch
-  //const multiplo5 = number % 5 === 0;
-  //const multiplo3 = number % 3 === 0;
-  //const multiploAmbos = multiplo5 && multiplo3;
-  //switch(){
-  //  case 1:
-  //    break;
-  //  default:
-  //    break;
-  //}
+
+  const multiplo5 = number % 5 === 0;
+  const multiplo3 = number % 3 === 0;
+  const multiploAmbos = multiplo5 && multiplo3;
+  
+  switch(true){
+    case multiploAmbos:
+      console.log("Múltiplo de 3 y 5");
+      break;
+    case multiplo3:
+      console.log("Múltiplo de 3");
+      break;
+    case multiplo5:
+      console.log("Múltiplo de 5");
+      break;
+    default:
+      console.log("No es múltiplo de 3 ni de 5");
+  }
 
 }
 
