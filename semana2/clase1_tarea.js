1bbc043c63af477ae474ab010cd20d347551e079
Bryan <bryanpiga1309@gmail.com>
2025-05-13 19:06:45 -0600

    Se añade ciclo for

--- semana2/clase1_tarea.js
+++ semana2/clase1_tarea.js
@@ -3,15 +3,26 @@
 const { ask } = require('../helpers/input');
 
 async function main() {
-
-    function sleep(ms) {
+  
+  function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
-    }
+  }
+  
+  let limite = Number(await ask("Hasta que numero quieres contar: "));
+  let i = 1;
+  
+//  while (i <= limite) {
+//    if(i%2===0){
+//        console.log(i);
+//        await sleep(1000);
+//    }else{
+//        console.log(`${i} no es par`)
+//        await sleep(1000);
+//    }
+//    i++;
+//  }
 
-    let limite = Number(await ask("Hasta que numero quieres contar: "));
-    let i = 1;
-
-  while (i <= limite) {
+  for(i; i<=limite;i++){
     if(i%2===0){
         console.log(i);
         await sleep(1000);
@@ -19,9 +30,8 @@ async function main() {
         console.log(`${i} no es par`)
         await sleep(1000);
     }
-    i++;
   }
-
+  
 }
 
 main();
